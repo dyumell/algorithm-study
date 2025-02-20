ac427d8f57a8e65872a46e5c0c54875000f956dd
dyumell <dyumelldev@gmail.com>
2025-02-20 23:36:50 +0900

    브론즈문제 1개 50점 받고 품

--- src/boj/class2/브론즈/boj15829.java
+++ src/boj/class2/브론즈/boj15829.java
@@ -24,13 +24,15 @@ public class boj15829 {
         for(int i =0; i< str.length();i++){
             for(int j = i; j > 0; j--) {
                 r[i] *= 31;
+                // 제곱 표현 개선 가능
+                // bigInt 대신 long 사용하여 복잡도를 줄일 수 있음
             }
         }
 
         for (int i = 0; i< str.length(); i++) {
-            result = ((str.charAt(i) - 48) * r[i]) % M;
+            result += ((str.charAt(i) - 97) * r[i]) % M;
         }
-
+        // 자바 프리미티브 자료형으로는 31의49승 표현 불가
         return result;
     }
 }
