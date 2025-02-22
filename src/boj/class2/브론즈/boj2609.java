d7fb2c56175f65e32eca5cc33c4cbc6da77b83df
dyumell <dyumelldev@gmail.com>
2025-02-22 23:09:12 +0900

    브론즈문제 못푼거 1개 품

--- src/boj/class2/브론즈/boj2609.java
+++ src/boj/class2/브론즈/boj2609.java
@@ -14,23 +14,29 @@ public class boj2609 {
         int b = Integer.parseInt(st.nextToken());
 
         if(a > b){
-            System.out.println(gdc(a,b));
+            System.out.println(gcd(a,b));
         } else {
-            System.out.println(gdc(b,a));
+            System.out.println(gcd(b,a));
         }
+        System.out.println(lcm(a,b));
 
     }
 
-    public static int gdc(int a,int b) {
+    public static int gcd(int a,int b) {
         while(true){
             if (a % b != 0) {
                 // 1071 은 1029 로 나뉘어 떯어지지 않기 댸문에
-                return gdc(b,a % b);
+                return gcd(b,a % b);
                 // 1071 을 1029 로 나눈 나머지를 구하여, 1029 를 42로 나눈다.
             } else {
                 // a % b == 0 : 1029 를 42로 나눈 나머지를 구한다 21, 42은 21로 나누어 떨어진다.
                 return b;
             }
         }
+    }
+
+    public static int lcm(int a,int b){
+        // 최소 공배수는 두 수를 곱하고 최대공약수로 나눈 것 과 같다.
+        return a * b / gcd(a,b);
     }
 }
